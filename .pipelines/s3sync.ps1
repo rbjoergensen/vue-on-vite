d0ab2be134aac1ea096f7cf69cc57cc4e55948a1
Rasmus Jørgensen <rbj@sampension.dk>
2022-08-14 22:52:33 +0200

    Initial

---
+++ .pipelines/s3sync.ps1
@@ -0,0 +1,4 @@
+aws s3 cp --content-type image/svg+xml --acl public-read dist/ s3://cotv-vue-test/ --metadata-directive REPLACE --exclude "*" --include "*.svg" --recursive
+aws s3 cp --content-type text/javascript --acl public-read dist/ s3://cotv-vue-test/ --metadata-directive REPLACE --exclude "*" --include "*.js" --recursive
+aws s3 cp --content-type text/css --acl public-read dist/ s3://cotv-vue-test/ --metadata-directive REPLACE --exclude "*" --include "*.css" --recursive
+aws s3 cp --content-type text/html --acl public-read dist/ s3://cotv-vue-test/ --metadata-directive REPLACE --exclude "*" --include "*.html" --recursive
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Vue 3 + TypeScript + Vite
+To setup and run debug.
+
+```
+npm create vite@latest
+npm install
+npm run dev
+```
+
+To build the app run `npm run build`. The output will be placed in the `dist` folder.
+
+To upload to S3 and test the site online run `.\.pipelines\s3sync.ps1` and visit the site at http://cotv-vue-test.s3-website.eu-central-1.amazonaws.com/
